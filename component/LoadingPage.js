3ed8263244c389c7627cce26fc5d270bfcb1bfaf
In Ha Ryu <iryu815@aucklanduni.ac.nz>
2021-07-14 22:58:36 +1200

    auto linting

--- component/LoadingPage.js
+++ component/LoadingPage.js
@@ -1,40 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
-
-import { AT_API_URL, AT_API_KEY } from "@env"
-
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { getVersion } from '../service/ATService';
 export default function LoadingView() {
-    const [version, setVersion] = useState("");
-    const [error, setError] = useState();
+    const [version, setVersion] = useState('');
 
-    useEffect(() => {
-        fetch(`${AT_API_URL}/v2/gtfs/versions`, {
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-                'Ocp-Apim-Subscription-Key': AT_API_KEY
-            }
-        })
-        .then(res => res.json())
-        .then(({status, response, error}) => {
-            setVersion(response[0].version)
-        })
-        .catch(err => {
-            console.log(err)
-            setError(err.toString());
-        })
-    }, [])
-    
+    useEffect(async () => {
+        const v = await getVersion();
+        setVersion(v);
+    }, []);
+
     return (
         <View style={styles.container}>
             <Text style={styles.text}>{version}</Text>
-            <Text style={styles.text}>
-                {error
-                ? error
-                : null}
-            </Text>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -44,10 +23,9 @@ const styles = StyleSheet.create({
         backgroundColor: '#ff6700',
         alignItems: 'center',
         justifyContent: 'center',
-        color: "#fff"
+        color: '#fff',
     },
     text: {
-        color: "#fff"
-    }
+        color: '#fff',
+    },
 });
-  
