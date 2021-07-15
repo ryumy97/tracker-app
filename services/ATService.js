757c5a19281175df6f4d28fffb2eb45737f1e431
ryumy97 <inha.ryu.97@gmail.com>
2021-07-15 22:46:33 +1200

    fetch version with axios get

--- services/ATService.js
+++ services/ATService.js
@@ -1,18 +1,18 @@
+import axios from 'axios';
+
 import { AT_API_URL, AT_API_KEY } from '@env';
 
 export async function getVersion() {
-    console.log(AT_API_URL, AT_API_KEY)
-    const version = await fetch(`${AT_API_URL}/v2/gtfs/versions`, {
+    const version = await axios.get(`${AT_API_URL}/v2/gtfs/versions`, {
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
             'Ocp-Apim-Subscription-Key': AT_API_KEY,
         },
     })
-    .then((res) => res.json())
-    .then(({ response }) => {
+    .then(({ data: { response } }) => {
         return response[0].version;
     });
-    console.log(version);
+
     return version;
 }
