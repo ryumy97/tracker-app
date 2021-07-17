b4333efd4f2d3a29fb1b5e0b1a7bbaa33f65f747
ryumy97 <inha.ryu.97@gmail.com>
2021-07-18 00:27:16 +1200

    getting location using expo location and render region of current location

--- components/MapPage.js
+++ components/MapPage.js
@@ -1,21 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Circle } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function MapPage() {
+    const [location, setLocation] = useState({});
+    const [errorMsg, setErrorMsg] = useState('');
+    const [region, setRegion] = useState({
+        latitude: -41.28666552,
+        longitude: 171.772996908,
+        latitudeDelta: 24,
+        longitudeDelta: 12,
+    });
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+
+    useEffect(() => {
+        if (location.coords) {
+            const {
+                coords: { latitude, longitude },
+            } = location;
+            setRegion((region) => {
+                return {
+                    ...region,
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.001,
+                    longitudeDelta: 0.005,
+                };
+            });
+        }
+    }, [location]);
+
     return (
         <View style={styles.container}>
-            <MapView
-                style={styles.map}
-                initialRegion={{
-                    latitude: -41.28666552,
-                    longitude: 171.772996908,
-                    latitudeDelta: 24,
-                    longitudeDelta: 12,
-                }}>
+            {errorMsg ? <Text>{errorMsg}</Text> : null}
+            <MapView style={styles.map} region={region}>
+                {location.coords ? (
+                    <Circle
+                        center={{
+                            latitude: location.coords.latitude,
+                            longitude: location.coords.longitude,
+                        }}
+                        radius={location.coords.accuracy | 1}
+                        strokeColor="rgba(50,180,255,0.25)"
+                        fillColor="rgba(50,180,255,0.1)"
+                        lineJoin="round"
+                    />
+                ) : null}
             </MapView>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -27,7 +73,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     map: {
-        ...StyleSheet.absoluteFillObject
-    }
+        ...StyleSheet.absoluteFillObject,
+    },
 });
-  
