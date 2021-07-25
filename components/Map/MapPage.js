30694a50f1b46f33f6088bffafe4b6d68a8b56c9
ryumy97 <inha.ryu.97@gmail.com>
2021-07-26 01:25:46 +1200

    modal open

--- components/Map/MapPage.js
+++ components/Map/MapPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Modal, Pressable, StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
 import { useLocation } from '../../contexts/LocationProvider';
 import CurrentLocationMarker from './CurrentLocationMarker';
@@ -12,6 +12,7 @@ export default function MapPage() {
     const { errorMsg, location, aspectRatio } = useLocation();
     const [currentRegion, setCurrentRegion] = useState({});
     const [isMapReady, setIsMapReady] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
     const mapRef = useRef(null);
     const searchBarRef = useRef(null);
 
@@ -30,6 +31,24 @@ export default function MapPage() {
     return (
         <View style={styles.container}>
             {errorMsg ? <Text>{errorMsg}</Text> : null}
+            <Modal
+                visible={modalVisible}
+                transparent={true}
+                statusBarTranslucent
+                animationType="fade"
+                onRequestClose={() => {
+                    setModalVisible(false);
+                }}
+            >
+                <Pressable style={styles.modalBackground} onPress={() => setModalVisible(false)}></Pressable>
+                <View style={styles.modal}>
+                    <View style={styles.card}>
+                        <Pressable style={styles.closeButton} onPress={() => setModalVisible(false)}>
+                            <Icon name="times" size={20} color="#aaa"></Icon>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
             <MapView
                 ref={mapRef}
                 style={styles.map}
@@ -54,9 +73,9 @@ export default function MapPage() {
             </MapView>
             <SafeAreaView style={styles.topWidgetContainer}>
                 <SearchBar ref={searchBarRef}></SearchBar>
-                <View style={styles.settingModalButton}>
-                    <Icon name="bars" size={20} color="#ff6700" />
-                </View>
+                <Pressable style={styles.settingModalButton} onPress={() => setModalVisible(true)}>
+                    <Icon name="cog" size={20} color="#ff6700" />
+                </Pressable>
             </SafeAreaView>
         </View>
     );
@@ -90,5 +109,34 @@ const styles = StyleSheet.create({
         borderRadius: 21,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    modal: {
+        justifyContent: 'center',
+        alignContent: 'center',
+        width: '100%',
+        height: '100%',
+        padding: 20,
+    },
+    modalBackground: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+    },
+    card: {
+        backgroundColor: '#fff',
+        width: '100%',
+        height: 500,
+        borderColor: '#ff6700',
+        borderRadius: 12,
+    },
+    closeButton: {
+        position: 'absolute',
+        margin: 10,
+        marginTop: 8,
+        top: 0,
+        right: 0,
     },
 });
